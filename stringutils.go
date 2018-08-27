d81bd7475f7bd4695f8c9613220968a4f46a62e2
vhemacademy <42744975+vhemacademy@users.noreply.github.com>
2018-08-27 12:17:35 -0400

    Create stringutils.go

---
+++ stringutils.go
@@ -0,0 +1,13 @@
+package gc2018stringutils
+
+import (
+	"strings"
+)
+
+func Uppers(s string) string {
+	return string.ToUpper(s)
+}
+
+func Lowers(s string) string {
+         return string.ToLower(s)
+}
